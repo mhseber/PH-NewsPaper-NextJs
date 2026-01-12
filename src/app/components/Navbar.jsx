125c6b882f7c42776869fdfab7cc0b9dadc41ce8
mahamudul hasan seber <sebermh@gmail.com>
2026-01-12 23:57:03 +0600

    Update Code

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>home</p>
+      <p>about</p>
+      <p>contact</p>
+    </div>
+  );
+};
+
+export default Navbar;
