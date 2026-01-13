3ad8c519fb4831c391560cdec6359b9a68f722dc
mahamudul hasan seber <sebermh@gmail.com>
2026-01-14 02:18:13 +0600

    Trending Section Update

--- src/app/home/Trending.jsx
+++ src/app/home/Trending.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Trending = () => {
-  return <div>Trending</div>;
+  return (
+    <section>
+      <div className="flex justify-center items-center text-red-900 font-semibold  bg-gray-200">
+        <span className="text-rotate text-7xl">
+          <span className="justify-items-center">
+            <span>BREAKING</span>
+            <span>REPORTING</span>
+            <span>UPDATING</span>
+            <span>INFORMING</span>
+            <span>ANALYZING</span>
+            <span>SHARING</span>
+          </span>
+        </span>
+      </div>
+    </section>
+  );
 };
 
 export default Trending;
